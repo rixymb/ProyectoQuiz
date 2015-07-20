6af32ea96d07b8cfd280d8f9db4cda30a591f6ba
Ricardo Martinez <rixymb@gmail.com>
2015-07-20 19:17:47 +0200

    Error corregido
    Faltaba un salto de carpeta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -7,9 +7,9 @@ exports.question = function(req, res) {
 // GET /quizes/answer
 exports.answer = function(req, res) {
   if (req.query.respuesta === 'Roma') {
-    res.render('quizes/answer', {respuesta: 'Correcto'});
+    res.render('../quizes/answer', {respuesta: 'Correcto'});
   }
   else{
-    res.render('quizes/answer', {respuesta: 'Incorrecto'});
+    res.render('../quizes/answer', {respuesta: 'Incorrecto'});
   }
 };

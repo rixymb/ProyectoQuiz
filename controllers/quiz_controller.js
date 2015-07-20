c75a867d12fd7d1191daaf7aed291059c2b8f924
Ricardo Martinez <rixymb@gmail.com>
2015-07-20 19:15:53 +0200

    Error
    Error en quiz_controller.js
    
    Faltaba un salto de carpeta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,7 +1,7 @@
 
 // GET /quizes/question
 exports.question = function(req, res) {
-  res.render('quizes/question', {pregunta: 'Capital de Italia'});
+  res.render('../quizes/question', {pregunta: 'Capital de Italia'});
 };
 
 // GET /quizes/answer
